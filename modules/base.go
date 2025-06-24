e462b66d8e1c456479799aecf61bb908a43463c0
fx0x55 <80245546+fx0x55@users.noreply.github.com>
2025-06-24 15:13:09 +0800

    refactor(model): add BaseWithDeleteAt struct

--- modules/base.go
+++ modules/base.go
@@ -2,15 +2,17 @@ package modules
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type Base struct {
-	ID        uint           `json:"id" gorm:"primarykey"`
-	CreatedAt time.Time      `json:"created_at" gorm:"comment:create time"`
-	UpdatedAt time.Time      `json:"updated_at" gorm:"comment:update time"`
-	DeletedAt gorm.DeletedAt `json:"deleted_at" gorm:"comment:delete time"`
+	ID        uint      `json:"id" gorm:"primarykey"`
+	CreatedAt time.Time `json:"created_at" gorm:"comment:create time"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"comment:update time"`
+}
+
+type BaseWithDeletedAt struct {
+	Base
+	DeletedAt *time.Time `json:"deleted_at" gorm:"comment:delete time"`
 }
 
 func (v *Base) GetId() uint {
