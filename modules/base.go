aea515ae92564ca371d87eb357c1cbfbc7babed9
fx0x55 <80245546+fx0x55@users.noreply.github.com>
2025-06-24 15:21:47 +0800

    delete BaseWithDeletedAt

--- modules/base.go
+++ modules/base.go
@@ -10,11 +10,6 @@ type Base struct {
 	UpdatedAt time.Time `json:"updated_at" gorm:"comment:update time"`
 }
 
-type BaseWithDeletedAt struct {
-	Base
-	DeletedAt *time.Time `json:"deleted_at" gorm:"comment:delete time"`
-}
-
 func (v *Base) GetId() uint {
 	return v.ID
 }
